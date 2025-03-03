60dd28ffa16599e2ab945efa5f247b64ac0841ca
P1470V <savva.kalashnov@gmail.com>
2025-03-03 10:30:39 +0100

    Configuration

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,19 +1,12 @@
 return {
-  "nvim-treesitter/nvim-treesitter", 
+  "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = {"lua", "c", "vim", "python", "rust"},
+      ensure_installed = {"lua", "c", "vim", "python", "rust", "norg"},
       highlight = { enable = true },
       indent = { enable = true },
     })
   end
 }
- 
- 
- 
- 
- 
- 
- 
